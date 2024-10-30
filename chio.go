cdf50f4853e49fc544d3f917c3f76c37ec0277ae
Lekuru <contact@lekuru.xyz>
2024-10-30 23:55:25 +0100

    Add initial `BanchoIO` interface

---
+++ chio.go
@@ -0,0 +1,89 @@
+package chio
+
+// BanchoPacket is a struct that represents a packet that
+// is sent or received
+type BanchoPacket struct {
+	PacketId int32
+	Data     interface{}
+}
+
+// BanchoIO is the interface that wraps the basic methods for
+// reading and writing packets to a Bancho client
+type BanchoIO interface {
+	// Write writes len(p) bytes from p to the underlying data stream
+	Write(p []byte) (n int, err error)
+
+	// Read reads up to len(p) bytes into p. It returns the number of bytes
+	Read(p []byte) (n int, err error)
+
+	// Close closes the underlying data stream
+	Close() error
+
+	// WritePacket writes a packet to the underlying data stream
+	WritePacket(packetId int32, data interface{}) error
+
+	// ReadPacket reads a packet from the underlying data stream
+	ReadPacket() (err error, packet *BanchoPacket)
+
+	// ImplementsPacket checks if the packetId is implemented in the client
+	ImplementsPacket(packetId int32) bool
+
+	WriteLoginReply(reply int32) error
+	WriteMessage(message Message) error
+	WritePing() error
+	WriteIrcChangeUsername(oldName string, newName string) error
+	WriteUserStats(info UserInfo) error
+	WriteUserQuit(quit UserQuit) error
+	WriteSpectatorJoined(userId uint32) error
+	WriteSpectatorLeft(userId uint32) error
+	WriteSpectateFrames(bundle ReplayFrameBundle) error
+	WriteVersionUpdate() error
+	WriteSpectatorCantSpectate(userId uint32) error
+	WriteGetAttention() error
+	WriteAnnounce(message string) error
+	WriteMatchUpdate(match Match) error
+	WriteMatchNew(match Match) error
+	WriteMatchDisband(matchId uint32) error
+	WriteLobbyJoin(userId uint32) error
+	WriteLobbyPart(userId uint32) error
+	WriteMatchJoinSuccess(match Match) error
+	WriteMatchJoinFail() error
+	WriteFellowSpectatorJoined(userId uint32) error
+	WriteFellowSpectatorLeft(userId uint32) error
+	WriteMatchStart(match Match) error
+	WriteMatchScoreUpdate(frame ScoreFrame) error
+	WriteMatchTransferHost() error
+	WriteMatchAllPlayersLoaded() error
+	WriteMatchPlayerFailed(slotId uint32) error
+	WriteMatchComplete() error
+	WriteMatchSkip() error
+	WriteUnauthorized() error
+	WriteChannelJoinSuccess(channel string) error
+	WriteChannelRevoked(channel string) error
+	WriteChannelAvailable(channel Channel) error
+	WriteChannelAvailableAutojoin(channel Channel) error
+	WriteBeatmapInfoReply(reply BeatmapInfoReply) error
+	WriteLoginPermissions(permissions uint32) error
+	WriteFriendsList(userIds []uint32) error
+	WriteProtocolNegotiation(version int32) error
+	WriteTitleUpdate(update TitleUpdate) error
+	WriteMonitor() error
+	WriteMatchPlayerSkipped(slotId int32) error
+	WriteUserPresence(presence UserInfo) error
+	WriteRestart(retryMs int32) error
+	WriteInvite(message Message) error
+	WriteChannelInfoComplete() error
+	WriteMatchChangePassword(password string) error
+	WriteSilenceInfo(timeRemaining int32) error
+	WriteUserSilenced(userId uint32) error
+	WriteUserPresenceSingle(info UserInfo) error
+	WriteUserPresenceBundle(infos []UserInfo) error
+	WriteUserDMsBlocked(targetName string) error
+	WriteTargetIsSilenced(targetName string) error
+	WriteVersionUpdateForced() error
+	WriteSwitchServer(t int32) error
+	WriteAccountRestricted() error
+	WriteRTX(message string) error
+	WriteMatchAbort() error
+	WriteSwitchTournamentServer(ip string) error
+}
