cbf8c05209fe75796d0db56a9d681ccfa067c427
Lekuru <contact@lekuru.xyz>
2024-10-31 19:09:23 +0100

    Only allow messages from #osu for b282

--- b282.go
+++ b282.go
@@ -144,6 +144,11 @@ func (client *b282) WriteLoginReply(reply int32) error {
 }
 
 func (client *b282) WriteMessage(message Message) error {
+	if message.Sender != "#osu" {
+		// Private messages & channels have not been implemented yet
+		return nil
+	}
+
 	writer := bytes.NewBuffer([]byte{})
 	writeString(writer, message.Sender)
 	writeString(writer, message.Content)
