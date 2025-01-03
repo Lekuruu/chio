b7da43aa9c45c4487d72f169e15a6895f3876fd8
Lekuru <contact@lekuru.xyz>
2025-01-03 01:29:34 +0100

    Fix match implementation in b312

--- b312.go
+++ b312.go
@@ -6,7 +6,8 @@ import (
 	"io"
 )
 
-// b312 adds more features to multiplayer
+// b312 adds the match start & update packets, as well
+// as the "InProgress" boolean inside the match struct
 type b312 struct {
 	BanchoIO
 	stream           io.ReadWriteCloser
@@ -250,6 +251,114 @@ func (client *b312) WriteMatchScoreUpdate(frame ScoreFrame) error {
 	return client.WritePacket(BanchoMatchScoreUpdate, writer.Bytes())
 }
 
+func (client *b312) WriteMatchUpdate(match Match) error {
+	writer := bytes.NewBuffer([]byte{})
+	client.WriteMatch(writer, match)
+	return client.WritePacket(BanchoMatchUpdate, writer.Bytes())
+}
+
+func (client *b312) WriteMatchNew(match Match) error {
+	writer := bytes.NewBuffer([]byte{})
+	client.WriteMatch(writer, match)
+	return client.WritePacket(BanchoMatchNew, writer.Bytes())
+}
+
+func (client *b312) WriteMatch(writer io.Writer, match Match) error {
+	slotsOpen := make([]bool, client.slotSize)
+	slotsUsed := make([]bool, client.slotSize)
+	slotsReady := make([]bool, client.slotSize)
+
+	for index, slot := range match.Slots {
+		slotsOpen[index] = slot.Status == SlotStatusOpen
+	}
+
+	for index, slot := range match.Slots {
+		slotsUsed[index] = slot.HasPlayer()
+	}
+
+	for index, slot := range match.Slots {
+		slotsReady[index] = slot.Status == SlotStatusReady
+	}
+
+	writeUint8(writer, uint8(match.Id))
+	writeBoolean(writer, match.InProgress)
+	writeUint8(writer, match.Type)
+	writeString(writer, match.Name)
+	writeString(writer, match.BeatmapText)
+	writeInt32(writer, match.BeatmapId)
+	writeString(writer, match.BeatmapChecksum)
+	writeBoolList(writer, slotsOpen)
+	writeBoolList(writer, slotsUsed)
+	writeBoolList(writer, slotsReady)
+
+	for _, slot := range match.Slots {
+		if slot.HasPlayer() {
+			writeInt32(writer, slot.UserId)
+		}
+	}
+
+	return nil
+}
+
+func (client *b312) ReadMatch(reader io.Reader) (Match, error) {
+	var err error
+	errors := NewErrorCollection()
+	match := Match{}
+
+	matchId, err := readUint8(reader)
+	match.Id = int32(matchId)
+	errors.Add(err)
+	match.InProgress, err = readBoolean(reader)
+	errors.Add(err)
+	match.Type, err = readUint8(reader)
+	errors.Add(err)
+	match.Name, err = readString(reader)
+	errors.Add(err)
+	match.BeatmapText, err = readString(reader)
+	errors.Add(err)
+	match.BeatmapId, err = readInt32(reader)
+	errors.Add(err)
+	match.BeatmapChecksum, err = readString(reader)
+	errors.Add(err)
+
+	match.Slots = make([]*MatchSlot, client.slotSize)
+	slotsOpen, err := readBoolList(reader)
+	errors.Add(err)
+	slotsUsed, err := readBoolList(reader)
+	errors.Add(err)
+	slotsReady, err := readBoolList(reader)
+	errors.Add(err)
+
+	for i := 0; i < client.slotSize; i++ {
+		slot := &MatchSlot{}
+		slot.Status = SlotStatusOpen
+		slot.Team = SlotTeamNeutral
+		slot.Mods = NoMod
+		slot.UserId = -1
+
+		if slotsOpen[i] {
+			slot.Status = SlotStatusOpen
+		}
+
+		if slotsUsed[i] {
+			slot.Status = SlotStatusNotReady
+		}
+
+		if slotsReady[i] {
+			slot.Status = SlotStatusReady
+		}
+
+		if slot.HasPlayer() {
+			slot.UserId, err = readInt32(reader)
+			errors.Add(err)
+		}
+
+		match.Slots[i] = slot
+	}
+
+	return match, errors.Next()
+}
+
 /* Inherited Packets */
 
 func (client *b312) WriteMessage(message Message) error {
@@ -308,10 +417,6 @@ func (client *b312) WriteStats(writer io.Writer, info UserInfo) error {
 	return client.previous.WriteStats(writer, info)
 }
 
-func (client *b312) WriteMatch(writer io.Writer, match Match) error {
-	return client.previous.WriteMatch(writer, match)
-}
-
 func (client *b312) WriteUserPresence(info UserInfo) error {
 	return client.previous.WriteUserPresence(info)
 }
@@ -330,14 +435,6 @@ func (client *b312) WriteGetAttention() error {
 
 func (client *b312) WriteAnnouncement(message string) error {
 	return client.previous.WriteAnnouncement(message)
-}
-
-func (client *b312) WriteMatchUpdate(match Match) error {
-	return client.previous.WriteMatchUpdate(match)
-}
-
-func (client *b312) WriteMatchNew(match Match) error {
-	return client.previous.WriteMatchNew(match)
 }
 
 func (client *b312) WriteMatchDisband(matchId uint32) error {
@@ -390,10 +487,6 @@ func (client b312) ReadScoreFrame(reader io.Reader) (*ScoreFrame, error) {
 
 func (client *b312) ReadFrameBundle(reader io.Reader) (*ReplayFrameBundle, error) {
 	return client.previous.ReadFrameBundle(reader)
-}
-
-func (client *b312) ReadMatch(reader io.Reader) (Match, error) {
-	return client.previous.ReadMatch(reader)
 }
 
 /* Unsupported Packets */
