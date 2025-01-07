313116676705477d8647cc7f7240f246ac787936
ascenttree <tree@ascension.wtf>
2025-01-08 06:50:04 +1100

    didn't see this

--- chio.go
+++ chio.go
@@ -115,7 +115,7 @@ type BanchoWriters interface {
 var clients map[int]BanchoIO = make(map[int]BanchoIO)
 
 const lowestVersion int = 282
-const highestVersion int = 338
+const highestVersion int = 340
 
 // GetClientInterface returns a BanchoIO interface for the given client version
 func GetClientInterface(stream io.ReadWriteCloser, clientVersion int) BanchoIO {
